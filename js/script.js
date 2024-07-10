bca3ef59b183173192b8451c71aff9bad69035a4
surefire-hu <surefire.hu2001@gmail.com>
2024-07-10 16:37:16 +0200

    introdotto bottone copia e alert

--- js/script.js
+++ js/script.js
@@ -9,6 +9,19 @@ let colorePreferito = prompt("Qual è il tuo colore preferito?");
 
 // creazione string risultato
 let risultato = nome + cognome + colorePreferito + '130';
+let risultato2 = cognome + nome + colorePreferito + '130';
+let risultato3 = nome + colorePreferito + cognome + '130';
+let risultato4 = nome + cognome + '130' + colorePreferito;
 
 // Mostra il risultato nella pagina
 document.getElementById("risultato").textContent = risultato;
+document.getElementById("risultato2").textContent = risultato2;
+document.getElementById("risultato3").textContent = risultato3;
+document.getElementById("risultato4").textContent = risultato4;
+
+function copyToClipboard(elementId) {
+    const text = document.getElementById(elementId).textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert("S.C.L.");
+    })
+}
