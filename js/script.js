883f003af0f22d457679a997b2b341ea773f4899
surefire-hu <surefire.hu2001@gmail.com>
2024-07-11 09:55:44 +0200

    cambio dichiarazione

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // il nome all'utente
-let nome = prompt("Qual è il tuo nome?");
+const nome = prompt("Qual è il tuo nome?");
 
 // il cognome all'utente
-let cognome = prompt("Qual è il tuo cognome?");
+const cognome = prompt("Qual è il tuo cognome?");
 
 // il colore preferito all'utente
-let colorePreferito = prompt("Qual è il tuo colore preferito?");
+const colorePreferito = prompt("Qual è il tuo colore preferito?");
 
 // creazione string risultato
 let risultato = nome + cognome + colorePreferito + '130';
